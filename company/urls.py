2d6bd372798d0052a0bd4c97dda76de9abb46e9a
Tejas Vaio <Tejas Vaio>
2017-01-08 23:38:11 +0530

    create employee view created

--- company/urls.py
+++ company/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
  	url(r'^employee_list', views.employee_list, name='employee_list'),
+ 	url(r'^employee_create', views.employee_create, name='employee_new'),
 ]
--- company/views.py
+++ company/views.py
@@ -5,6 +5,11 @@ from django.forms import ModelForm
 
 from company.models import Employee
 
+class EmployeeForm(ModelForm):
+    class Meta:
+        model = Employee
+        fields = ['first_name', 'last_name', 'designation']
+
 def index(request):
 	return HttpResponse("<h1>This is the company app home page<h1>")
 
@@ -13,3 +18,11 @@ def employee_list(request, template_name='company_template/employee_list.html'):
     data = {}
     data['object_list'] = employee
     return render(request, template_name, data)
+
+
+def employee_create(request, template_name='company_template/employee_form.html'):
+    form = EmployeeForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('/company/employee_list.html')
+    return render(request, template_name, {'form':form})
